6867df304fee5f88a5e8fa5e4a49d8a3bb453c6e
Ahmed Emad <ahmeedemadmohamed@gmail.com>
2026-02-04 04:47:22 +0200

    add env example and update config

--- src/app/(home)/questions/_components/footer-btn-result.tsx
+++ src/app/(home)/questions/_components/footer-btn-result.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button'
 import { Compass, RotateCcw } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 export default function FooterBtnResult() {
@@ -11,9 +12,14 @@ export default function FooterBtnResult() {
             <RotateCcw className="w-4 h-4" />
             Restart
         </Button>
-        <Button className="flex-1 gap-2 bg-blue-600 hover:bg-blue-700 cursor-pointer">
-            <Compass className="w-4 h-4" />
-            Explore
+        <Button
+            asChild
+            className="flex-1 gap-2 bg-blue-600 hover:bg-blue-700 cursor-pointer"
+        >
+            <Link href="/diplomas">
+                <Compass className="w-4 h-4" />
+                Explore
+            </Link>
         </Button>
     </div >
 }
